8f16de494d76f320f197f86b521f2118f7d84500
José Igor <j.igor.ifce@gmail.com>
2020-10-21 09:13:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from api.app_restx import app
 from data_acquisition.data_creation import populate_database
 import os
 
-populate_database()
-
-app.run(debug=True)
+if __name__ == '__main__':
+  # creating and populating database, if it not exists
+  populate_database()
+  # running api
+  app.run(debug=True)
